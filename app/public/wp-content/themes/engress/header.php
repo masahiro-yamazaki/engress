41d42fd76bb16d771839a9fbe03522459c19c8b4
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2021-11-17 18:44:36 +0900

    feat: header.phpとfooter.phpを作成し、CSSとJSを読み込む対応を追加

---
+++ app/public/wp-content/themes/engress/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja" prefix="og: http://ogp.me/ns#">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="format-detection" content="telephone=no">
+
+  <title>TF-30</title>
+  <meta name="description" content="">
+
+  <meta property="og:title" content="Engress">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://example.com/">
+  <meta property="og:image" content="https://example.com/img/ogp.png">
+  <meta property="og:site_name" content="Engress">
+  <meta property="og:description" content="">
+  <meta name="twitter:card" content="summary_large_image">
+
+  <?php wp_head(); ?>
+  <link rel="icon" href="./img/icon-home.png">
+</head>
+
+<body>
+  <!-- ヘッダー -->
+  <header>
+  </header>
--- app/public/wp-content/themes/engress/index.php
+++ app/public/wp-content/themes/engress/index.php
@@ -1,26 +1,4 @@
-<meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="format-detection" content="telephone=no">
-
-  <title>TF-30</title>
-  <meta name="description" content="">
-
-  <meta property="og:title" content="Engress">
-  <meta property="og:type" content="website">
-  <meta property="og:url" content="https://example.com/">
-  <meta property="og:image" content="https://example.com/img/ogp.png">
-  <meta property="og:site_name" content="Engress">
-  <meta property="og:description" content="">
-  <meta name="twitter:card" content="summary_large_image">
-
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css?ver=5.8.2">
-  <link rel="stylesheet" href="./css/style.css">
-  <link rel="icon" href="./img/icon-home.png">
-
-</head>
-
-<body>
-  <!-- ヘッダー -->
-  <header>
-  </header>
-</body>
+<?php get_header(); ?>
+  <!-- CSSが有効になっているかどうかテスト -->
+  <h1 class='test-title'>Engress</h1>
+<?php get_footer(); ?>
