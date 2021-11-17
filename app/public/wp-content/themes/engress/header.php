0ec74b8aca1356eddb8c28b07489c81e5e5ae58e
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2021-11-17 19:15:27 +0900

    feat: ヘッダーのロゴを作成

--- app/public/wp-content/themes/engress/header.php
+++ app/public/wp-content/themes/engress/header.php
@@ -1,27 +1,42 @@
 <!DOCTYPE html>
-<html lang="ja" prefix="og: http://ogp.me/ns#">
+<html lang='ja' prefix='og: http://ogp.me/ns#'>
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="format-detection" content="telephone=no">
+  <meta charset='UTF-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <meta name='format-detection' content='telephone=no'>
 
-  <title>TF-30</title>
-  <meta name="description" content="">
+  <title>Engress</title>
+  <meta name='description' content=''>
 
-  <meta property="og:title" content="Engress">
-  <meta property="og:type" content="website">
-  <meta property="og:url" content="https://example.com/">
-  <meta property="og:image" content="https://example.com/img/ogp.png">
-  <meta property="og:site_name" content="Engress">
-  <meta property="og:description" content="">
-  <meta name="twitter:card" content="summary_large_image">
+  <meta property='og:title' content='Engress'>
+  <meta property='og:type' content='website'>
+  <meta property='og:url' content='https://example.com/'>
+  <meta property='og:image' content='https://example.com/img/ogp.png'>
+  <meta property='og:site_name' content='Engress'>
+  <meta property='og:description' content=''>
+  <meta name='twitter:card' content='summary_large_image'>
 
   <?php wp_head(); ?>
-  <link rel="icon" href="./img/icon-home.png">
+  <link rel='icon' href='./img/icon-home.png'>
 </head>
 
 <body>
   <!-- ヘッダー -->
-  <header>
-  </header>
+  <header class='ly_header'>
+    <div class='ly_header_inner'>
+      <a class='bl_headerLogo' href='#'>
+        <img src='<?php echo get_template_directory_uri() ?>/img/header-logo.png' alt='Engress' class='el_headerLogoImg'>
+      </a>
+      <!-- 
+      <div class='header-menu'>
+        <a href='#features'>特徴</a>
+        <a href='#price'>価格</a>
+        <a href='#contact'>お問い合わせ</a>
+      </div>
+      <a class='header-contact' href='#contact'>
+        <span>お問い合わせ</span>
+      </a>
+      -->
+    </div><!-- /.ly_header_inner -->
+  </header><!-- /.ly_header -->
