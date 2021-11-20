460c46d58366a4fb381660e3d4599f23cd47fdc3
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2021-11-20 23:24:33 +0900

    feat: ファーストビューのサブタイトルを追加

--- app/public/wp-content/themes/engress/index.php
+++ app/public/wp-content/themes/engress/index.php
@@ -4,6 +4,10 @@
   <div class='hp_fv'>
     <div class='ly_fv_inner'>
       <div class='bl_fvTitle'>TOEFL対策はEngress</div>
+      <div class='bl_fvSubTitle'>
+        <p>日本人へのTOEFL指導歴豊かな講師陣の</p>
+        <p>コーチング型TOEFLスクール</p>
+      </div>
     </div><!-- /.ly_fv_inner -->
   </div><!-- /.hp_fv -->
   <div style='height: 81px; background-color: blue;'></div>
