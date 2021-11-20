d3b22ccfa39ad2779d2bbff95d48be96bc9de7c7
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2021-11-20 22:53:04 +0900

    feat: ファーストビューのタイトルを追加

--- app/public/wp-content/themes/engress/index.php
+++ app/public/wp-content/themes/engress/index.php
@@ -1,8 +1,11 @@
 <?php get_header(); ?>
 <main>
   <!-- ファーストビュー -->
-  <div class='hp_firstView'>
-  </div>
+  <div class='hp_fv'>
+    <div class='ly_fv_inner'>
+      <div class='bl_fvTitle'>TOEFL対策はEngress</div>
+    </div><!-- /.ly_fv_inner -->
+  </div><!-- /.hp_fv -->
   <div style='height: 81px; background-color: blue;'></div>
   <div style='height: 81px; background-color: green;'></div>
   <div style='height: 500px; background-color: blue;'></div>
