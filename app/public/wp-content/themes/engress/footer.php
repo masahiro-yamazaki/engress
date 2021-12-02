62f8fe2efdb84e7534e2ff321e7670f594fb2344
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2021-12-02 19:17:43 +0900

    feat: copyrightを追加

--- app/public/wp-content/themes/engress/footer.php
+++ app/public/wp-content/themes/engress/footer.php
@@ -4,6 +4,13 @@
       <a href='' class='bl_telItem_number'>0123-456-7890</a>
       <div class='bl_telItem_time'>平日 08:00~20:00</div>
     </section>
+    <section class='bl_footerMenu'>
+    </section>
+    <section class='ly_copyright'>
+      <div class='ly_copyright_inner'>
+        <p class='bl_copyright_text'>© 2020 Engress.</p>
+      </div>
+    </section>
   </footer>
   <?php wp_footer(); ?>
 </body>
