1316b504f5ab7706f69b74e82524e1b95059210e
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2021-12-02 19:34:35 +0900

    feat: フッターメニューを追加

--- app/public/wp-content/themes/engress/footer.php
+++ app/public/wp-content/themes/engress/footer.php
@@ -5,6 +5,38 @@
       <div class='bl_telItem_time'>平日 08:00~20:00</div>
     </section>
     <section class='bl_footerMenu'>
+      <div class='bl_footerMenu_inner'>
+        <div class='bl_footerNav_items'>
+          <nav>
+            <ul class='bl_footerNav'>
+                <li class='bl_footerNav_item'>
+                  <a class='bl_footerNav_link' href=''>ホーム</a>
+                </li>
+                <li class='bl_footerNav_item'>
+                  <a class='bl_footerNav_link' href=''>お知らせ</a>
+                </li>
+                <li class='bl_footerNav_item'>
+                  <a class='bl_footerNav_link' href=''>ブログ</a>
+                </li>
+                <li class='bl_footerNav_item'>
+                  <a class='bl_footerNav_link' href=''>コース・料金</a>
+                </li>
+              </ul>
+          </nav>
+        </div>
+        <div class='bl_footerMenu_info'>
+          <a class='bl_footerMenu_logo' href='#'>
+            <img src='<?php echo get_template_directory_uri() ?>/img/header-logo.png' alt='Engress'>
+          </a>
+          <a class='bl_footer_tel'>
+            <img src='<?php echo get_template_directory_uri() ?>/img/footer-tel-icon.png' alt='tel'>
+            <span>0123-456-7890</span>
+          </a>
+          <div class='bl_footer_time'>
+            平日08:00~20:00
+          </div>
+        </div>
+      </div>
     </section>
     <section class='ly_copyright'>
       <div class='ly_copyright_inner'>
