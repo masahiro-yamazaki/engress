5f64d84e590b89f6a0c14300e6c3604da6aed665
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2021-12-04 22:34:09 +0900

    ブログ一覧ページpage-blog.phpを作成

---
+++ app/public/wp-content/themes/engress/page-blog.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+<main>
+  <section style='height: 300px; background-color: blue;'>
+    <h1>ブログ一覧</h1>
+  </section>
+  <section style='height: 300px; background-color: green;'>
+  </section>
+  <!-- 資料請求 -->
+  <section class='bl_document'>
+    <h2 class='bl_document_title'>まずは無料で資料請求から</h2>
+    <div class='bl_document_ctaButton_wrapper'>
+      <a class='bl_document_ctaButton'>資料請求</a>
+    </div>
+    <div class='bl_document_contactLink_wrapper'>
+      <a href='' class='bl_document_contactLink'>お問い合わせ</a>
+    </div>
+  </section>
+</main>
+<?php get_footer(); ?>
