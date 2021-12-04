1538852049a548a1bf3139720efa8f1011f81e5f
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2021-12-04 22:29:02 +0900

    feat: home.phpを作成

---
+++ app/public/wp-content/themes/engress/home.php
@@ -0,0 +1,166 @@
+<?php get_header(); ?>
+<main>
+  <!-- ファーストビュー -->
+  <section class='hp_fv'>
+    <div class='ly_fv_inner'>
+      <h1 class='bl_fvTitle'>TOEFL対策はEngress</h2>
+      <p class='bl_fvSubTitle'>日本人へのTOEFL指導歴豊かな講師陣の<br>コーチング型TOEFLスクール</p>
+      <div class='bl_fvCtaBtn_wrapper'>
+        <a class='el_ctaBtn el_ctaBtn__orange el_ctaBtn__rounded hp_ft16'>資料請求</a>
+      </div>
+      <div class='bl_fvContactLink'>
+        <a href='' class='el_contactLink'>お問い合わせ</a>
+      </div>
+    </div><!-- /.ly_fv_inner -->
+  </section><!-- /.hp_fv -->
+  <!-- こんなお悩みありませんか？ -->
+  <section class='hp_problem'>
+    <div class='ly_problem_inner'>
+      <h2 class='el_title el_title__white'>TOEFL学習で<br class='md_only'>こんな悩みありませんか？</h2>
+      <div class='bl_problemItems'>
+        <p class='bl_problemItem'>勉強の習慣が<br class='lg_only'>身についていない</p>
+        <p class='bl_problemItem'>勉強しているはず<br class='lg_only'>なのに点数が伸びない</p>
+        <p class='bl_problemItem'>正しい勉強方法が<br class='lg_only'>分からない</p>
+      </div>
+      <div class='bl_feature'>
+        <div class='bl_feature_inner'>
+          <p class='bl_featureTitle'>Engressは<br><span class='hp_underline'>TOEFL対策に特化したスクール</span>です</p>
+          <p class='bl_featureDescription'>完全オーダーメイドで、１人１人の悩みに合わせた最適な指導で<br class='lg_only'>TOEFLの苦手分野を克服します。</p>
+        </div>
+      </div>
+    </div><!-- /.ly_problem_inner -->
+  </section><!-- /.hp_problem -->
+  <!-- Engress3つの強み -->
+  <section class='ly_strengths'>
+    <div class='ly_strengths_inner'>
+      <div class='bl_strengths_ttl'>
+        <h2 class='el_title el_title__navy'>TOEFL対策に特化した<br class='md_only'>Engress3つの強み</h2>
+      </div>
+      <div class='bl_media'>
+        <figure class='bl_media_imgWrapper'>
+          <img src='<?php echo get_template_directory_uri() ?>/img/feature01.png'>
+        </figure>
+        <div class='bl_media_body'>
+          <div class='bl_media_label'>特長 １</div>
+          <h3 class='bl_media_ttl'>TOEFLに最適化された<br>無駄のないカリキュラム</h3>
+          <p class='bl_media_txt'>TOEFLではビジネス英語には登場しない数多くの学術的内容が出題されます。そのため、ベースとなる知識も必要になります。Engressでは過去1000題を分析し、最適なカリキュラムを組んでいます。</p>
+        </div>
+      </div>
+      <div class='bl_media bl_media__reverse'>
+        <figure class='bl_media_imgWrapper'>
+          <img src='<?php echo get_template_directory_uri() ?>/img/feature02.png'>
+        </figure>
+        <div class='bl_media_body'>
+          <div class='bl_media_label'>特長 ２</div>
+          <h3 class='bl_media_ttl'>日本人指導歴10年以上の<br>経験豊富な講師陣</h3>
+          <p class='bl_media_txt'>TOEFLではビジネス英語には登場しない数多くの学術的内容が出題されます。そのため、ベースとなる知識も必要になります。Engressでは過去1000題を分析し、最適なカリキュラムを組んでいます。</p>
+        </div>
+      </div>
+      <div class='bl_media'>
+        <figure class='bl_media_imgWrapper'>
+          <img src='<?php echo get_template_directory_uri() ?>/img/feature03.png'>
+        </figure>
+        <div class='bl_media_body'>
+          <div class='bl_media_label'>特長 ３</div>
+          <h3 class='bl_media_ttl'>平均3ヶ月でTOEFL <br class='md_only'>iBT20点アップ</h3>
+          <p class='bl_media_txt'>TOEFLではビジネス英語には登場しない数多くの学術的内容が出題されます。そのため、ベースとなる知識も必要になります。Engressでは過去1000題を分析し、最適なカリキュラムを組んでいます。</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- 料金プラン -->
+  <section class='ly_price'>
+    <div class='ly_price_inner'>
+      <!-- TODO: タイトルのCSSを共通化 -->
+      <h2 class='bl_price_ttl'>Engressの料金プランはこちら</h2>
+      <a href='' class='bl_price_btn'>料金を見てみる</a>
+    </div>
+  </section>
+  <!-- 成功事例 -->
+  <section class='ly_success'>
+    <div class='ly_success_inner'>
+      <h2 class='bl_success_title'>TOEFL成功事例</h2>
+      <div class='bl_success_examples'>
+        <div class='bl_success_example'>
+          <h3 class='bl_success_summary'>TOEFL iBT 100点を突破してコロンビア大学大学院に進学できました！</h3>
+          <!-- <h3 class='bl_success_summary'><?php get_field('title'); ?></h3> -->
+          <div class='bl_success_picture'></div>
+          <div class='bl_success_profile'>
+            <div class='bl_success_status'>会社員</div>
+            <div class='bl_success_name'>T.Fujiyamaさん</div>
+          </div>
+          <div class='bl_success_result'>3ヶ月でTOEFL80→108点</div>
+        </div>
+        <div class='bl_success_example'>
+          <h3 class='bl_success_summary'>TOEFL iBT 100点を突破してコロンビア大学大学院に進学できました！</h3>
+          <div class='bl_success_picture'></div>
+          <div class='bl_success_profile'>
+            <div class='bl_success_status'>会社員</div>
+            <div class='bl_success_name'>T.Fujiyamaさん</div>
+          </div>
+          <div class='bl_success_result'>3ヶ月でTOEFL80→108点</div>
+        </div>
+        <div class='bl_success_example'>
+          <h3 class='bl_success_summary'>TOEFL iBT 100点を突破してコロンビア大学大学院に進学できました！</h3>
+          <div class='bl_success_picture'></div>
+          <div class='bl_success_profile'>
+            <div class='bl_success_status'>会社員</div>
+            <div class='bl_success_name'>T.Fujiyamaさん</div>
+          </div>
+          <div class='bl_success_result'>3ヶ月でTOEFL80→108点</div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- ご利用の流れ -->
+  <section class='ly_flow'>
+    <div class='ly_flow_inner'>
+      <div class='bl_flow_title'>
+        <h2 class='el_title el_title__navy'>ご利用の流れ</h2>
+      </div>
+      <div class='bl_flow_parts'>
+        <div class='bl_flow_part'>
+          <div class='bl_flow_number'>01</div>
+          <div class='bl_flow_summary_content'>
+            <div class='bl_flow_summary'><h3>お問い合わせ</h3></div>
+            <div class='bl_flow_content'>まずはフォームまたはお電話からお申し込みください。</div>
+          </div>
+        </div>
+        <div class='bl_flow_part'>
+          <div class='bl_flow_number'>02</div>
+          <div class='bl_flow_summary_content'>
+            <div class='bl_flow_summary'><h3>ヒアリング</h3></div>
+            <div class='bl_flow_content'>現在の学習状況やTOEFLスコア、目標のスコアをお聞きします。</div>
+          </div>
+        </div>
+        <div class='bl_flow_part'>
+          <div class='bl_flow_number'>03</div>
+          <div class='bl_flow_summary_content'>
+            <div class='bl_flow_summary'><h3>学習プランのご提供</h3></div>
+            <div class='bl_flow_content'>目標達成のために最適な学習プランをご提案致します。</div>
+          </div>
+        </div>
+        <div class='bl_flow_part'>
+          <div class='bl_flow_number'>04</div>
+          <div class='bl_flow_summary_content'>
+            <div class='bl_flow_summary'><h3>ご入会</h3></div>
+            <div class='bl_flow_content'>お申込み完了後、レッスンがスタートします。</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div style='height: 81px; background-color: blue;'></div>
+  <div style='height: 81px; background-color: green;'></div>
+  <!-- 資料請求 -->
+  <section class='bl_document'>
+    <h2 class='bl_document_title'>まずは無料で資料請求から</h2>
+    <div class='bl_document_ctaButton_wrapper'>
+      <a class='bl_document_ctaButton'>資料請求</a>
+    </div>
+    <div class='bl_document_contactLink_wrapper'>
+      <a href='' class='bl_document_contactLink'>お問い合わせ</a>
+    </div>
+  </section>
+</main>
+<?php get_footer(); ?>
--- app/public/wp-content/themes/engress/single.php
+++ app/public/wp-content/themes/engress/single.php
@@ -1,1 +1,44 @@
-<h1>single.php</h1>
+<?php get_header(); ?>
+
+<!-- content -->
+<div id="content">
+<div class="inner">
+
+<!-- primary -->
+<main id="primary">
+
+<?php if ( function_exists( 'bcn_display' ) ) : ?>
+<!-- breadcrumb -->
+<div class="breadcrumb">
+<?php bcn_display(); ?>
+</div><!-- /breadcrumb -->
+<?php endif; ?>
+
+<?php
+if ( have_posts() ) :
+while ( have_posts() ) :
+the_post();
+?>
+
+<!-- entry -->
+<article class="entry">
+</article> <!-- /entry -->
+
+<?php
+endwhile;
+endif;
+?>
+
+</main><!-- /primary -->
+
+<?php get_sidebar(); ?>
+
+</div><!-- /inner -->
+</div><!-- /content -->
+
+<section style='height: 500px; background-color: blue;'>
+  <h1>single.php</h1>
+</section>
+<section style='height: 500px; background-color: green;'>
+</section>
+<?php get_footer(); ?>
