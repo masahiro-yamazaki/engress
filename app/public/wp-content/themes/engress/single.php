7a954a649244b449f1d8f4c419af1e83f29a38b4
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2021-12-15 19:18:01 +0900

    feat: ブログ詳細ページにサイドバーを追加

--- app/public/wp-content/themes/engress/single.php
+++ app/public/wp-content/themes/engress/single.php
@@ -1,5 +1,4 @@
 <?php get_header(); ?>
-<main>
   <?php if( !(is_home() || is_front_page() )): ?>
     <section class='ly_breadcrumb'>
       <div class='ly_inner bl_breadcrumb'>
@@ -12,40 +11,49 @@
     </section>
   <?php endif; ?>
 
-  <!-- ブログ記事 -->
-  <section>
-    <div class="entry-header">
-      <?php
-        $category = get_the_category();
-        if ( $category[0] ) : ?>
-        <div class="entry-label">
-          <a href="<?php echo esc_url( get_category_link( $category[0]->term_id ) ); ?>"><?php echo $category[0]->cat_name; ?></a>
-        </div>
-      <?php endif; ?>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
+  <div class='ly_content'>
+    <div class='ly_inner'>
+      <main>
+        <!-- ブログ記事 -->
+        <section>
+          <div class="entry-header">
+            <?php
+              $category = get_the_category();
+              if ( $category[0] ) : ?>
+              <div class="entry-label">
+                <a href="<?php echo esc_url( get_category_link( $category[0]->term_id ) ); ?>"><?php echo $category[0]->cat_name; ?></a>
+              </div>
+            <?php endif; ?>
+            <h1 class="entry-title"><?php the_title(); ?></h1>
 
-      <div class="entry-meta">
-        <time class="entry-published" datetime="<?php the_time( 'c' ); ?>">公開日 <?php the_time( 'Y/n/j' ); ?></time>
-        <?php if ( get_the_modified_time( 'Y-m-d' ) !== get_the_time( 'Y-m-d' ) ) : ?>
-          <time class="entry-updated" datetime="<?php the_modified_time( 'c' ); ?>">最終更新日 <?php the_modified_time( 'Y/n/j' ); ?></time>
-        <?php endif; ?>
-      </div>
+            <div class="entry-meta">
+              <time class="entry-published" datetime="<?php the_time( 'c' ); ?>">公開日 <?php the_time( 'Y/n/j' ); ?></time>
+              <?php if ( get_the_modified_time( 'Y-m-d' ) !== get_the_time( 'Y-m-d' ) ) : ?>
+                <time class="entry-updated" datetime="<?php the_modified_time( 'c' ); ?>">最終更新日 <?php the_modified_time( 'Y/n/j' ); ?></time>
+              <?php endif; ?>
+            </div>
 
-      <div class="entry-img">
-        <?php
-          if ( has_post_thumbnail() ) {
-            the_post_thumbnail( 'large' );
-          }
-        ?>
-      </div>
+            <div class="entry-img">
+              <?php
+                if ( has_post_thumbnail() ) {
+                  the_post_thumbnail( 'large' );
+                }
+              ?>
+            </div>
+          </div>
+
+          <?php the_content(); ?>
+        </section>
+
+        <section style='height: 80px; background-color: green;'>
+          <span>TODO: おすすめ記事</span>
+        </section>
+      </main>
+      <aside class='ly_sidebar lg_only'>
+      </aside>
     </div>
+  </div>
 
-    <?php the_content(); ?>
-  </section>
-
-  <section style='height: 80px; background-color: green;'>
-    <span>TODO: おすすめ記事</span>
-  </section>
   <!-- 資料請求 -->
   <section class='ly_document'>
     <div class='ly_inner'>
@@ -58,5 +66,4 @@
       </div>
     </div>
   </section>
-</main>
 <?php get_footer(); ?>
