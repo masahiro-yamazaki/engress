f6a583a605f2b269a75bc39e8d140b46b6e28543
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2021-12-19 00:14:55 +0900

    feat: ブログ一覧ページを固定ページで設定

--- app/public/wp-content/themes/engress/functions.php
+++ app/public/wp-content/themes/engress/functions.php
@@ -22,6 +22,16 @@ function my_setup()
 
 add_action('after_setup_theme', 'my_setup');
 
+/* 投稿アーカイブページの作成 */
+function post_has_archive($args, $post_type) {
+  if ( 'post' == $post_type ) {
+    $args['rewrite'] = true;
+    $args['has_archive'] = 'blogs'; //任意のスラッグ名
+  }
+  return $args;
+}
+add_filter( 'register_post_type_args', 'post_has_archive', 10, 2 );
+
 /**
 * CSSとJavaScriptの読み込み
 */
