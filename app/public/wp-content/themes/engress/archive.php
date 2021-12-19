0d34b5955814409893166fe8f0727ef5d65cdcbb
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2021-12-19 22:04:21 +0900

    feat: ブログ一覧ページのページネーションを追加

--- app/public/wp-content/themes/engress/archive.php
+++ app/public/wp-content/themes/engress/archive.php
@@ -69,7 +69,18 @@
         <?php endwhile; endif; ?>
       </div>
       <!-- ページネーション -->
-      <!-- ページネーション -->
+      <?php if (paginate_links()) : ?>
+        <div class='bl_pagination'>
+          <?php
+            $args = array(
+              'end_size' => 1,
+              'mid_size' => 2,
+              'prev_next' => false
+            );
+            echo paginate_links($args);
+          ?>
+        </div>
+      <?php endif; ?>
     </div>
   </section>
   <!-- 資料請求 -->
