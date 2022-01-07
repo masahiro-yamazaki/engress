3fa9a88af10c58022eb732b1ad40381f076e2698
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2022-01-07 18:59:58 +0900

    feat: SP画面で料金プランがスクロールするように修正

--- app/public/wp-content/themes/engress/page-price.php
+++ app/public/wp-content/themes/engress/page-price.php
@@ -31,54 +31,56 @@
   <section class='ly_plan'>
     <div class='ly_inner'>
       <h2 class='bl_price_title'>料金表</h2>
-      <div class='bl_plans'>
-        <div class='bl_plan'>
-          <h3 class='bl_plan_title'>基礎プラン</h3>
-          <div class='bl_plan_body'>
-            <p class='bl_plan_price'>59,000円〜</p>
-            <p class='bl_plan_remark'>*月額（税抜価格）</p>
-            <ul class='bl_plan_contents'>
-              <li class='bl_plan_content bl_plan_content__checked'>カリキュラム作成</li>
-              <li class='bl_plan_content bl_plan_content__checked'>TOEFL学習サポート</li>
-              <li class='bl_plan_content bl_plan_content__checked'>週一回のビデオMTG</li>
-            </ul>
+      <div class='bl_plans_wrapper'>
+        <div class='bl_plans'>
+          <div class='bl_plan'>
+            <h3 class='bl_plan_title'>基礎プラン</h3>
+            <div class='bl_plan_body'>
+              <p class='bl_plan_price'>59,000円〜</p>
+              <p class='bl_plan_remark'>*月額（税抜価格）</p>
+              <ul class='bl_plan_contents'>
+                <li class='bl_plan_content bl_plan_content__checked'>カリキュラム作成</li>
+                <li class='bl_plan_content bl_plan_content__checked'>TOEFL学習サポート</li>
+                <li class='bl_plan_content bl_plan_content__checked'>週一回のビデオMTG</li>
+              </ul>
+            </div>
           </div>
-        </div>
-        <div class='bl_plan'>
-          <h3 class='bl_plan_title'>演習プラン</h3>
-          <div class='bl_plan_body'>
-            <p class='bl_plan_price'>75,000円〜</p>
-            <p class='bl_plan_remark'>*月額（税抜価格）</p>
-            <ul class='bl_plan_contents'>
-              <li class='bl_plan_content bl_plan_content__checked'>カリキュラム作成</li>
-              <li class='bl_plan_content bl_plan_content__checked'>TOEFL学習サポート</li>
-              <li class='bl_plan_content bl_plan_content__checked'>週一回のビデオMTG</li>
-              <li class='bl_plan_content bl_plan_content__checked'>月二回の模試（解説付き）</li>
-            </ul>
+          <div class='bl_plan'>
+            <h3 class='bl_plan_title'>演習プラン</h3>
+            <div class='bl_plan_body'>
+              <p class='bl_plan_price'>75,000円〜</p>
+              <p class='bl_plan_remark'>*月額（税抜価格）</p>
+              <ul class='bl_plan_contents'>
+                <li class='bl_plan_content bl_plan_content__checked'>カリキュラム作成</li>
+                <li class='bl_plan_content bl_plan_content__checked'>TOEFL学習サポート</li>
+                <li class='bl_plan_content bl_plan_content__checked'>週一回のビデオMTG</li>
+                <li class='bl_plan_content bl_plan_content__checked'>月二回の模試（解説付き）</li>
+              </ul>
+            </div>
           </div>
-        </div>
-        <div class='bl_plan'>
-          <h3 class='bl_plan_title bl_plan_title__recommended'>おすすめ<br>志望校対策プラン</h3>
-          <div class='bl_plan_body'>
-            <p class='bl_plan_price bl_plan_price__recommended'>95,000円〜</p>
-            <p class='bl_plan_remark'>*月額（税抜価格）</p>
-            <ul class='bl_plan_contents'>
-              <li class='bl_plan_content bl_plan_content__checked'>カリキュラム作成</li>
-              <li class='bl_plan_content bl_plan_content__checked'>TOEFL学習サポート</li>
-              <li class='bl_plan_content bl_plan_content__checked'>週一回のビデオMTG</li>
-              <li class='bl_plan_content bl_plan_content__checked'>月二回の模試（解説付き）</li>
-              <li class='bl_plan_content bl_plan_content__checked'>週一の英語面接対策</li>
-            </ul>
+          <div class='bl_plan'>
+            <h3 class='bl_plan_title bl_plan_title__recommended'>おすすめ<br>志望校対策プラン</h3>
+            <div class='bl_plan_body'>
+              <p class='bl_plan_price bl_plan_price__recommended'>95,000円〜</p>
+              <p class='bl_plan_remark'>*月額（税抜価格）</p>
+              <ul class='bl_plan_contents'>
+                <li class='bl_plan_content bl_plan_content__checked'>カリキュラム作成</li>
+                <li class='bl_plan_content bl_plan_content__checked'>TOEFL学習サポート</li>
+                <li class='bl_plan_content bl_plan_content__checked'>週一回のビデオMTG</li>
+                <li class='bl_plan_content bl_plan_content__checked'>月二回の模試（解説付き）</li>
+                <li class='bl_plan_content bl_plan_content__checked'>週一の英語面接対策</li>
+              </ul>
+            </div>
           </div>
-        </div>
-        <div class='bl_plan'>
-          <h3 class='bl_plan_title'>フレックスプラン</h3>
-          <div class='bl_plan_body'>
-            <p class='bl_plan_price'>60,000円〜</p>
-            <p class='bl_plan_remark'>*月額（税抜価格）</p>
-            <ul class='bl_plan_contents'>
-              <li class='bl_plan_content'>＊別途ご相談ください</li>
-            </ul>
+          <div class='bl_plan'>
+            <h3 class='bl_plan_title'>フレックスプラン</h3>
+            <div class='bl_plan_body'>
+              <p class='bl_plan_price'>60,000円〜</p>
+              <p class='bl_plan_remark'>*月額（税抜価格）</p>
+              <ul class='bl_plan_contents'>
+                <li class='bl_plan_content'>＊別途ご相談ください</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
