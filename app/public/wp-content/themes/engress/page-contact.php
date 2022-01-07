b0d2c721394a3fd807eea4c8dddce1be99d1f1e7
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2022-01-07 19:30:04 +0900

    お問い合わせのファーストビューを作成

--- app/public/wp-content/themes/engress/page-contact.php
+++ app/public/wp-content/themes/engress/page-contact.php
@@ -1,5 +1,21 @@
 <?php get_header(); ?>
 <main>
-  <h1 class='contact-test'>お問い合わせ・資料請求</h1>
+  <!-- ファーストビュー -->
+  <section class='ly_contactFv'>
+    <div class='ly_inner'>
+      <h1 class='el_fvTitle'>ブログ</h1>
+    </div>
+  </section>
+  <?php if( !(is_home() || is_front_page() )): ?>
+    <section class='ly_breadcrumb'>
+      <div class='ly_inner bl_breadcrumb'>
+        <?php
+        if ( function_exists('bcn_display') ) {
+          bcn_display();
+        }
+        ?>
+      </div>
+    </section>
+  <?php endif; ?>
 </main>
 <?php get_footer(); ?>
