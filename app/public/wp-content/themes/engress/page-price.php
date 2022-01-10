61f7285cb02744953076c54b94178397b11ce0fd
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2022-01-10 12:56:11 +0900

    feat: 各プランの料金をカスタムフィールドで設定

--- app/public/wp-content/themes/engress/page-price.php
+++ app/public/wp-content/themes/engress/page-price.php
@@ -36,7 +36,7 @@
           <div class='bl_plan'>
             <h3 class='bl_plan_title'>基礎プラン</h3>
             <div class='bl_plan_body'>
-              <p class='bl_plan_price'>59,000円〜</p>
+              <p class='bl_plan_price'><?php echo number_format(get_field('basic')); ?>円〜</p>
               <p class='bl_plan_remark'>*月額（税抜価格）</p>
               <ul class='bl_plan_contents'>
                 <li class='bl_plan_content bl_plan_content__checked'>カリキュラム作成</li>
@@ -48,7 +48,7 @@
           <div class='bl_plan'>
             <h3 class='bl_plan_title'>演習プラン</h3>
             <div class='bl_plan_body'>
-              <p class='bl_plan_price'>75,000円〜</p>
+              <p class='bl_plan_price'><?php echo number_format(get_field('exercise')); ?>円〜</p>
               <p class='bl_plan_remark'>*月額（税抜価格）</p>
               <ul class='bl_plan_contents'>
                 <li class='bl_plan_content bl_plan_content__checked'>カリキュラム作成</li>
@@ -61,7 +61,7 @@
           <div class='bl_plan'>
             <h3 class='bl_plan_title bl_plan_title__recommended'>おすすめ<br>志望校対策プラン</h3>
             <div class='bl_plan_body'>
-              <p class='bl_plan_price bl_plan_price__recommended'>95,000円〜</p>
+              <p class='bl_plan_price bl_plan_price__recommended'><?php echo number_format(get_field('preferred_school')); ?>円〜</p>
               <p class='bl_plan_remark'>*月額（税抜価格）</p>
               <ul class='bl_plan_contents'>
                 <li class='bl_plan_content bl_plan_content__checked'>カリキュラム作成</li>
@@ -75,7 +75,7 @@
           <div class='bl_plan'>
             <h3 class='bl_plan_title'>フレックスプラン</h3>
             <div class='bl_plan_body'>
-              <p class='bl_plan_price'>60,000円〜</p>
+              <p class='bl_plan_price'><?php echo number_format(get_field('flex')); ?>円〜</p>
               <p class='bl_plan_remark'>*月額（税抜価格）</p>
               <ul class='bl_plan_contents'>
                 <li class='bl_plan_content'>＊別途ご相談ください</li>
